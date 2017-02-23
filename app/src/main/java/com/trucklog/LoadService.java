b0d5b8f777d7a694b1ddae74c92cffabc0d8e810
varindersingh83 <varinder83singh@gmail.com>
2017-02-22 20:32:58 -0800

    Update LoadService.java

--- app/src/main/java/com/trucklog/LoadService.java
+++ app/src/main/java/com/trucklog/LoadService.java
@@ -32,7 +32,7 @@ public class LoadService extends Service {
 
     private Socket mSocket;{
         try {
-            mSocket = IO.socket("https://urbantucking.com");///socket.io
+            mSocket = IO.socket("https://urbantrucking.com");///socket.io
         } catch (URISyntaxException e) {}
     }
 
